2048c2ca9fa908907237059054397b8cae03fefc
Mirzalazuardi H <mirzalazuardi@gmail.com>
2021-09-25 07:23:08 +0700

    add stimulus

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.js"
+pin "@hotwired/stimulus", to: "stimulus.js"
+pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
